c756095c70025af8dae48adfe809e2135abbb4ea
0atx <1771360@hansung.ac.kr>
2022-04-08 11:29:38 +0900

    220401 Solved BOJ_DP_S3_11726

---
+++ src/com/ssafy/algorithm/DP/BOJ_DP_S3_11726.java
@@ -0,0 +1,44 @@
+package com.ssafy.algorithm.DP;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+ * @author 0at_x
+ * @since 2022. 04. 01.
+ * @see https://www.acmicpc.net/problem/11726
+ * @category #DP
+ */
+
+public class BOJ_DP_S3_11726 {
+
+	private static int N;
+	private static int[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		N = Integer.parseInt(br.readLine());
+
+		dp = new int[1001];
+
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+		}
+
+		bw.write(dp[N] + "\n");
+
+		br.close();
+		bw.flush();
+		bw.close();
+
+	}
+
+}
