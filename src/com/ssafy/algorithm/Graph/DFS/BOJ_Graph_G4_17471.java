423ff74eac925791c2913b87ee912e2cf9f71fe6
Yunha <55011885+0atx@users.noreply.github.com>
2022-04-10 22:40:09 +0900

    220406 Solved BOJ_Graph_G4_17471

--- src/com/ssafy/algorithm/Graph/DFS/BOJ_Graph_G4_17471.java
+++ src/com/ssafy/algorithm/Graph/DFS/BOJ_Graph_G4_17471.java
@@ -56,32 +56,20 @@ public class BOJ_Graph_G4_17471 {
 		bw.close();
 	}
 
-	/**
-	 * 크기에 따라 요소를 선택해보자!!
-	 * 
-	 * @param toChoose 선택해야 할(남은) 개수
-	 * @param choosed  선택 결과
-	 * @param start    출발점
-	 * @param size     선택 해야하는 개수
-	 */
 	private static void combination(int toChoose, boolean[] choosed, int start, int size) {
 		if (toChoose == 0) {
-			// true인 그룹 탐색해보기 - 어디서 시작하지?
 			int si = getStart(choosed, true);
 			findCnt = 0;
 			int pop1 = dfs(si, choosed, new boolean[N], true);
 
-			// 완탐 실패!!
 			if (findCnt != size) {
 				return;
 			}
 
-			// false인 그룹 탐색해보기 - 어디서 시작하지?
 			si = getStart(choosed, false);
 			findCnt = 0;
 			int pop2 = dfs(si, choosed, new boolean[N], false);
 
-			// 완탐 실패!!
 			if (findCnt != ((N - 1) - size)) {
 				return;
 			}
@@ -98,22 +86,11 @@ public class BOJ_Graph_G4_17471 {
 		}
 	}
 
-	/**
-	 * 
-	 * @param i       방문하는 idx
-	 * @param choosed 구성 정보
-	 * @param visited 방문 여부
-	 * @param check   탐색할 값
-	 * @return 탐색 결과 인구수의 합
-	 */
 	private static int dfs(int i, boolean[] choosed, boolean[] visited, boolean check) {
-		// 1. 방문 표시
 		visited[i] = true;
-		// 2. 할 일 처리
-		int pop = pops[i]; // 인구 가져오기
+		int pop = pops[i];
 		findCnt++;
 
-		// 3. 자식 탐색 - 미방문, 연결되어있고, check일 것
 		for (int c = 1; c < N; c++) {
 			if (!visited[c] && graph[i][c] && choosed[c] == check) {
 				pop += dfs(c, choosed, visited, check);
@@ -123,13 +100,6 @@ public class BOJ_Graph_G4_17471 {
 		return pop;
 	}
 
-	/**
-	 * 배열에서 check가 처음 나온 시점을 반환해보자.
-	 * 
-	 * @param choosed
-	 * @param check
-	 * @return
-	 */
 	private static int getStart(boolean[] choosed, boolean check) {
 		for (int c = 1; c < choosed.length; c++) {
 			if (choosed[c] == check) {
