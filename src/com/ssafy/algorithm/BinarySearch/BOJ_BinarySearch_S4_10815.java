9d6859c3149272a0b402a5dffdc46f35053d82ac
yhpark <1771360@hansung.ac.kr>
2022-06-30 01:31:19 +0900

    220317 Solved BOJ_BinarySearch_S4_10815

--- src/com/ssafy/algorithm/BinarySearch/BOJ_BinarySearch_S4_10815.java
+++ src/com/ssafy/algorithm/BinarySearch/BOJ_BinarySearch_S4_10815.java
@@ -1,4 +1,4 @@
-package com.ssafy.algorithm.BS;
+package com.ssafy.algorithm.BinarySearch;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -15,7 +15,7 @@ import java.util.StringTokenizer;
  * @category #BinarySearch
  */
 
-public class BOJ_BS_S4_10815 {
+public class BOJ_BinarySearch_S4_10815 {
 
 	private static int N, M;
 	private static int[] cards;
